371ad8e16c389e144f225564342211f040e767bd
Joseph Gambino <joe@gam.bingo>
2021-12-10 17:14:57 -0600

    budgetgit add -u!

--- budget_game.py
+++ budget_game.py
@@ -3,7 +3,7 @@ import streamlit as st
 import src.budget_logic as lg
 from src.budget_sidebar import sidebar
 from src.budget_config import (BIZNESS_SPACES, COMMON_SPACES, TECH_SPACES, 
-    ACADEMIC_SPACES, AMENITIES)
+    ACADEMIC_SPACES, CUSTOM_SPACES)
 
 
 st.set_page_config(
@@ -18,8 +18,7 @@ sidebar()
 
 st.header("Design Your DPI")
 msg = """
-    Choose what to include in your ideal Discovery Partners Institute. But, 
-    careful! We don't have unlimited budget.
+    Choose what to include in your ideal Discovery Partners Institute.
 """
 st.write(msg)
 col1, col2, _, col3 = st.columns([2, 2, 1, 5])
@@ -27,9 +26,13 @@ lg.space_sliders(col1, "Shared Spaces", COMMON_SPACES)
 lg.space_sliders(col2, "Tech Talent Development", TECH_SPACES)
 lg.space_sliders(col2, "Applied R&D", ACADEMIC_SPACES)
 lg.space_sliders(col2, "Entrepreneurial", BIZNESS_SPACES)
-lg.amenity_checkboxes(col2, "Amenities")
+lg.amenity_checkboxes(col1, "Amenities")
+lg.define_your_own(col2, "Custom Spaces", CUSTOM_SPACES)
 
+budget_cap = lg.calculate_budget_cap()
+col3.write(f"Be careful. You only have ${budget_cap} to spend!")
 col3.metric("You've spent", f"${st.session_state['spend']}")
+lg.total_spend(col3, budget_cap, st.session_state["spend"])
 ttl = col3.text_input("Name your piece!", value="My 2D Discovery Partners Institute")
 name = col3.text_input("Sign your work!")
 lg.treemap(col3, ttl, name)
--- src/budget_logic.py
+++ src/budget_logic.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import squarify
+import pandas as pd
 
-from .budget_config import (BIZNESS_SPACES, COMMON_SPACES, TECH_SPACES, 
-    ACADEMIC_SPACES, AMENITIES, OPTIONS)
+from .budget_config import CUSTOM_SPACES, AMENITIES, OPTIONS
 
 
 def initialize_session_state():
@@ -48,20 +48,108 @@ def amenity_checkboxes(st_col, title):
         update_budget()
 
 
+def define_your_own(st_col, title, config):
+    with st_col.expander(title, expanded=True):
+        new_space = st.text_input("Define a New Space")
+        new_space = new_space.title().replace("'S", "'s")
+
+        if new_space not in config and new_space != "":
+            specs = {
+                "price":    30,
+                "qty":      0,
+                "size":     2,
+                "color":    "#A4A680",
+            }
+            config[new_space] = specs
+            OPTIONS[new_space] = specs
+            st.session_state[f"{new_space}_price"] = 30
+
+        for option in config:
+            quantity_slider(option, config)
+        update_budget()
+
+
+def calculate_budget_cap():
+    limit = 2
+    budget_cap = sum(OPTIONS[space]["price"]*limit for space in OPTIONS)
+    budget_cap = int(round(budget_cap / 100.0)) * 100
+    return budget_cap
+
+
+def total_spend(st_col, budget_cap, total_spend):
+    df = pd.DataFrame(columns=["Spend", "Legend", "Limit", "Order", "Budget"])
+    df = df.append({
+        "Spend": "", 
+        "Legend": "Under Budget", 
+        "Limit": budget_cap*0.8,
+        "Budget": total_spend,
+        "Order": 0}, ignore_index=True)
+    df = df.append({
+        "Spend": "", 
+        "Legend": "Cutting it Close", 
+        "Limit": budget_cap*0.2,
+        "Budget": total_spend,
+        "Order": 1}, ignore_index=True)
+    
+    if total_spend > budget_cap:
+        df = df.append({
+            "Spend": "", 
+            "Legend": "Over Budget", 
+            "Limit": total_spend-budget_cap, 
+            "Budget": total_spend,
+            "Order": 2}, ignore_index=True)
+
+    print(df)
+
+    color_codes = {
+        "Under Budget": "#94C343CC",
+        "Cutting it Close": "#E0A21DCC",
+        "Over Budget": "#C01214CC",
+    }
+    labels = sorted(df["Legend"].value_counts().index.tolist())
+    colors = [color_codes[l] for l in labels]
+
+    spec1 = {
+        "mark": "bar",
+        "encoding": {
+            "color": {
+                "field": "Legend",
+                "scale": {"range": colors}},
+            "x": {"aggregate": "sum", "field": "Limit", "title": "Budget"},
+            "y": {"field": "Spend"},
+            "order": {"field": "Order", "type": "ordinal"}
+        }
+    }
+    spec2 = {
+        "mark": "rule",
+        "encoding": {
+            # "x": {"aggregate": "mean", "field": "Budget"},
+            "x": {"datum": total_spend, "title": ""},
+            "color": {"value": "black"},
+            "size": {"value": 3}
+            }
+    }
+    spec = {
+        "layer": [spec1, spec2],
+        # "title": f"Your Budget is ${budget_cap}"}
+    }
+    st_col.vega_lite_chart(df, spec, use_container_width=True)
+
+
 def treemap(st_col, ttl, name):
     spaces, sizes, colors = [], [], []
-    categories = [BIZNESS_SPACES, COMMON_SPACES, TECH_SPACES, ACADEMIC_SPACES]
-    for category in categories:
-        for room_type in category:
-            qty = category[room_type]["qty"]
-            if qty:
-                spaces += [room_type]*qty
-                sizes += [category[room_type]["size"]]*qty
-                colors += [category[room_type]["color"]]*qty
+    all_spaces = [room for room in OPTIONS if room not in AMENITIES]
+
+    for room_type in all_spaces:
+        qty = OPTIONS[room_type]["qty"]
+        if qty:
+            spaces += [room_type]*qty
+            sizes += [OPTIONS[room_type]["size"]]*qty
+            colors += [OPTIONS[room_type]["color"]]*qty
     
     spaces = [spc.replace(" ", "\n") for spc in spaces]
 
-    fig, ax = plt.subplots()
+    fig, _ = plt.subplots()
     plt.rc('font', size=8)
     squarify.plot(sizes=sizes, label=spaces, color=colors, alpha=0.7, pad=True)
     # squarify.plot(sizes=sizes, label=spaces, alpha=0.7, pad=True)
